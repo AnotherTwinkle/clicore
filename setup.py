cc3497dfe29830d7c9b02442e93b1f3db4a6b9b2
AnotherTwinkle <anothersparklingstar@gmail.com>
2022-02-05 16:09:02 +0600

    Prepare commands lib for seperate release

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@
 
 from setuptools import setup, find_packages
 
-setup(name = "cftool",
+setup(name = "clicore",
         version = "0.2",
         author = "AnotherTwinkle",
-        url = "https://www.github.com/AnotherTwinkle/cftool",
-        packages = find_packages(),
+        url = "https://www.github.com/AnotherTwinkle/clicore",
+        packages = ['clicore'],
         include_package_data = True,
-        entry_points = {'console_scripts' : ['cftool=cftool.main:main']}
         )
         
